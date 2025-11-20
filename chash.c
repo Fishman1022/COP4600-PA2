df2779c3bd3a1f53380cacbb0f9fb2c93a551822
Broc <thebattlefrog@gmail.com>
2025-11-20 16:34:24 -0500

    hashInsert, hashDelete, hash function

--- chash.c
+++ chash.c
@@ -1,5 +1,6 @@
 #include "parser.h"
 #include "threadlock.h"
+#include <string.h>
 #include <stdint.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -12,8 +13,7 @@ long long current_timestamp() {
   return microseconds;  
 }
 
-typedef struct hash_struct
-{
+typedef struct hash_struct {
   uint32_t hash;
   char name[50];
   uint32_t salary;
@@ -22,49 +22,67 @@ typedef struct hash_struct
 
 //Organizes Hash records into an array, lets us track if one is locked/released
 typedef struct {
-    hashRecord *head;
-    pthread_rwlock_t rwlock;
+  hashRecord *head;
+  pthread_rwlock_t rwlock;
 } hashBucket;
 
 //Hash Table Wrapper
 typedef struct {
-    hashBucket *buckets;
-    size_t num_buckets;
+  hashBucket *buckets;
+  size_t num_buckets;
 } concurrentHashTable;
 
-concurrentHashTable* createHashTable(size_t num_buckets){
-    //Creates table, then initializes num buckets and creates array structure for each bucket
-    concurrentHashTable* table = malloc(sizeof(concurrentHashTable));
-    table->num_buckets = num_buckets;
-    table->buckets = malloc(num_buckets * sizeof(hashBucket));
+concurrentHashTable* createHashTable(size_t num_buckets) {
+  //Creates table, then initializes num buckets and creates array structure for each bucket
+  concurrentHashTable* table = (concurrentHashTable *)malloc(sizeof(concurrentHashTable));
+  table->num_buckets = num_buckets;
+  table->buckets = malloc(num_buckets * sizeof(hashBucket));
 
-    //Initializes each bucket
-    for (int i = 0; i < num_buckets; i++)
-    {
-        table->buckets->head = NULL;
-        pthread_mutex_init(&table->buckets[i].rwlock, NULL);
-    }
+  //Initializes each bucket
+  for (int i = 0; i < num_buckets; i++) {
+    table->buckets->head = NULL;
+    pthread_rwlock_init(&table->buckets[i].rwlock, NULL);
+  }
 
-    return table;
+  return table;
+}
+
+// Jenkins one at a time hash (Copied from https://en.wikipedia.org/wiki/Jenkins_hash_function)
+// per assignment details.
+uint32_t jenkins_one_at_a_time_hash(char * name) { 
+  size_t i = 0;
+  uint32_t hash = 0;
+  while (i != strlen(name)) {
+    hash += name[i++];
+    hash += hash << 10;
+    hash ^= hash >> 6;
+  }
+  hash += hash << 3;
+  hash ^= hash >> 11;
+  hash += hash << 15;
+  return hash;
 }
 
 //These are template, you can change them to whateverr yoou think wroks best lol
-void readCommandsFile("commands.txt");
+void readCommandsFile();
 void threadRelease(concurrentHashTable* table, uint32_t hash, int isWriter, int priority);
 void threadLock(concurrentHashTable* table);
 void hashInsert(concurrentHashTable* table, char* name, int salary, int priority);
-void hashDelete(concurrentHashTable * table, char* name,  int priority);
+void hashDelete(concurrentHashTable* table, char* name, int priority);
 void hashSearch(concurrentHashTable* table, char* name, int priority);
 void hashPrint(concurrentHashTable* table);
 void writeLogFile();
 
-
 int main() {
-
   concurrentHashTable* table = createHashTable(100);
+
+  hashInsert(table, "Broc", 0, 1);
+  hashDelete(table, "Broc", 1);
+
+  return 0;
 }
 
-void threadRelease(concurrentHashTable* table, uint32_t hash, int isWriter, int priority){
+void threadRelease(concurrentHashTable* table, uint32_t hash, int isWriter, int priority) {
   if (!table) return;
 
   size_t index = hash %table->num_buckets;
@@ -76,5 +94,71 @@ void threadRelease(concurrentHashTable* table, uint32_t hash, int isWriter, int 
     printf("%lld, THREAD %d WRITE LOCK RELEASED\n", timestamp, priority);
   } else {
     printf("%lld, THREAD %d READ LOCK RELEASED\n", timestamp, priority);
+  }
+}
+
+void hashInsert(concurrentHashTable* table, char* name, int salary, int priority) {
+  if (!table) return;
+
+  uint32_t hashVal = jenkins_one_at_a_time_hash(name);
+  size_t index = hashVal % table->num_buckets;
+
+  pthread_rwlock_wrlock(&table->buckets[index].rwlock);
+
+  hashRecord* current = table->buckets[index].head;
+  while (current != NULL) {
+    if (current->hash == hashVal) {
+      printf("Insert failed. Entry %d is a duplicate.", hashVal);
+      pthread_rwlock_unlock(&table->buckets[index].rwlock);
+      return;
+    }
+    current = current->next;
+  }
+
+  hashRecord* rec = (hashRecord*) malloc(sizeof(hashRecord));
+  strcpy(rec->name, name);
+  rec->salary = salary;
+  rec->hash = hashVal;
+
+  rec->next = table->buckets[index].head;
+  table->buckets[index].head = rec;
+
+  pthread_rwlock_unlock(&table->buckets[index].rwlock);
+
+  printf("Inserted %d,%s,%d\n", hashVal, name, salary);
+}
+
+void hashDelete(concurrentHashTable* table, char* name, int priority) {
+  if (!table) return;
+
+  uint32_t hashVal = jenkins_one_at_a_time_hash(name);
+  size_t index = hashVal % table->num_buckets;
+
+  pthread_rwlock_wrlock(&table->buckets[index].rwlock);
+
+  hashRecord* current = table->buckets[index].head;
+  hashRecord* prev = NULL;
+  int found = 0;
+
+  while (current != NULL) {
+    if (strcmp(current->name, name) == 0) {
+      found = 1;
+      if (prev == NULL) {
+        table->buckets[index].head = current->next;
+      } else {
+        prev->next = current->next;
+      }
+      printf("Deleted record for %d,%s,%d\n", hashVal, name, current->salary);
+      free(current);
+      break;
+    }
+    prev = current;
+    current = current->next;
+  }
+
+  pthread_rwlock_unlock(&table->buckets[index].rwlock);
+
+  if (!found) {
+    printf("Entry %d not deleted. Not in database.\n", hashVal);
   }
 }
